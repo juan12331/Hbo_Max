2232905e68a1016b0a664bc41cc79b5c7b6083f9
juan12331 <juanxadez2@gmail.com>
2024-12-03 14:56:14 -0300

    começando appbar (to com vontade de fazer isso só nas ferias)

--- frontend/src/pages/landing/landing.jsx
+++ frontend/src/pages/landing/landing.jsx
@@ -1,11 +1,23 @@
 import react from 'react'
-import './langing.css'
+import './landing.css'
 
 function Landing() {
 
   return (
     <>
-      
+      <div className="landingPage">
+        <nav className='navBar'>
+        
+          <div className="image">
+            <img src="https://www.max.com/img/max-h-w-l.svg" alt="hbo max image" className='hbo'/>
+          </div>
+          <div className="logar">
+            <div className="translate">
+              <img src="frontend\src\assets\translation.png" alt="asdsad" className='transtale'/>
+            </div>
+          </div>
+        </nav>
+      </div>
     </>
   )
 }
