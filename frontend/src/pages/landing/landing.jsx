0b381f517434cc46cbca6134a24623de67ea7693
juan12331 <juanxadez2@gmail.com>
2024-12-05 14:41:51 -0300

    terminando a navbar

--- frontend/src/pages/landing/landing.jsx
+++ frontend/src/pages/landing/landing.jsx
@@ -12,15 +12,21 @@ function Landing() {
           <div className="image">
             <img src="https://www.max.com/img/max-h-w-l.svg" alt="hbo max image" className='hbo'/>
           </div>
-          <div className="logar">
+         <div className="botoes">
+         <div className="logar">
             <button className="translate">
               <img src={translation} alt="asdsad" className='transtale'/>
               <span className='translateText'>TRADUZIR</span>
             </button>
-            <button>
+            
+          </div>
+          <button className='translate'>
               <span className='translateText'>ENTRAR</span>
             </button>
-          </div>
+            <button className='translateBlue'>
+              <span className='translateText'>ASSINE AGORA</span>
+            </button>
+         </div>
         </nav>
       </div>
     </>
