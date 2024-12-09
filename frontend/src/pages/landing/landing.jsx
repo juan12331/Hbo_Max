22a20a27cf1dbb137e9765f821cb4a624b64e7bb
juan12331 <juanxadez2@gmail.com>
2024-12-09 14:40:31 -0300

    .

--- frontend/src/pages/landing/landing.jsx
+++ frontend/src/pages/landing/landing.jsx
@@ -9,29 +9,43 @@ function Landing() {
     <>
       <div className="landingPage">
         <nav className='navBar'>
-        
+
           <div className="image">
-            <img src="https://www.max.com/img/max-h-w-l.svg" alt="hbo max image" className='hbo'/>
+            <img src="https://www.max.com/img/max-h-w-l.svg" alt="hbo max image" className='hbo' />
           </div>
-         <div className="botoes">
-         <div className="logar">
-            <button className="translate">
-              <img src={translation} alt="asdsad" className='transtale'/>
-              <span className='translateText'>TRADUZIR</span>
-            </button>
-            
-          </div>
-          <button className='translate'>
+          <div className="botoes">
+            <div className="logar">
+              <button className="translate">
+                <img src={translation} alt="asdsad" className='transtale' />
+                <span className='translateText'>TRADUZIR</span>
+              </button>
+
+            </div>
+            <button className='translate'>
               <span className='translateText'>ENTRAR</span>
             </button>
             <button className='translateBlue'>
               <span className='translateText'>ASSINE AGORA</span>
             </button>
-         </div>
+          </div>
         </nav>
 
         <div className="hero">
-          <div className="pretoEncima"></div>
+          <div className="pretoEncima">
+
+            <div className="image">
+
+              <img src="https://www.max.com/img/max-h-w-l.svg" alt="hbo max image" className='hbo' />
+            </div>
+
+            <div className="text">MUITO MAIS A VER</div>
+            <div className="text2">Planos a partir de <strong>R$18,90 / mês</strong></div>
+
+            <button className='translateBlue'>
+              <span className='translateText'>ASSINE AGORA</span>
+            </button>
+
+          </div>
           <img src={films} alt="" className='films' />
         </div>
       </div>
